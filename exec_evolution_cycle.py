08fba616a4e05ab6f2d14a98c8447bf8225e06fa
lucas.silva1337@gmail.com <131429Alfaomeg@>
2017-10-05 18:39:00 -0300

    Last modifications, 04/10/2017
    Remodeled evolution_cycle.py

--- exec_evolution_cycle.py
+++ exec_evolution_cycle.py
@@ -1,163 +1,4 @@
-import new_genetic_ops as gops
-import individuo_complex as ind
-import numpy as np
-import copy as copy
-
-def evolution_cycle(alphabet, logs, size_pop, pop_exchange, max_generations, weights_fit, crossover_setup,
-                    mutation_setup, selection_setup, elitism, max_len_trace, set_quant, exec_id):
-
-    # First i create the initial population
-    pop = [[ind.criarIndividuo(alphabet), {'f':0,'c':0,'p':0}] for _ in range(size_pop)]
-    # Calculating the fitness for the first population
-    for individual in pop:
-        individual[1] = gops.fitness(individuo=individual[0], logs=logs, max_len_trace=max_len_trace, set_quant=set_quant, weights=weights_fit)[1]
-
-    # Some lists to keep the evolution of the run
-    fit_evol = []
-    best_ind_evol = []
-    # Calculando os dados de fitness pros gráficos
-    aux = [val[1]['f'] for val in pop]
-    min_fit = min(aux)
-    max_fit = max(aux)
-    average = sum(aux) / len(aux)
-    max_index = aux.index(max_fit)
-    # Sorting the population so i can get the best fitness
-    # sorted_pop = sorted(initial_pop, key=lambda t: t[1]['f'])
-    # Keeping the evolution stats
-    best_ind_evol.append(pop[max_index])
-    fit_evol.append([min_fit, max_fit, average])
-
-    # Beginning of the evolution cycle
-    aux_gen = 0
-    while max_generations > aux_gen:
-        print('Geração ', aux_gen)
-        new_pop = []
-        # Elitism of the previous population
-        if np.random.random() <= elitism:
-            # A way to get the max fitness individual without sorting
-            aux = [foo[1]['f'] for foo in pop]
-            max_aux = max(aux)
-            max_index = aux.index(max_aux)
-            new_pop.append(pop[max_index])
-        # Filling the new pop
-        while len(new_pop) < len(pop):
-            # Selection
-            if selection_setup['tournament']==1:
-                dad = gops.tournament_selection(pop)
-                mom = gops.tournament_selection(pop)
-            elif selection_setup['roulette']==1:
-                dad = gops.roulette_selection(pop)
-                mom = gops.roulette_selection(pop)
-            else:
-                print('incorrect setup for selection')
-                exit()
-            # Crossover
-            if np.random.random() <= crossover_setup['chance']:
-                result = gops.crossover(dad[0], mom[0], crossover_setup['points'])
-                son_1 = [result[0], {'f':0,'c':0,'p':0}]
-                son_2 = [result[1], {'f':0,'c':0,'p':0}]
-            else:
-                son_1 = copy.deepcopy(dad)
-                son_2 = copy.deepcopy(mom)
-            # Mutation
-            if np.random.random() <= mutation_setup['logic']:
-                gops.mutation_logic(son_1[0])
-                gops.mutation_logic(son_2[0])
-            if np.random.random() <= mutation_setup['complex']:
-                gops.mutation_complexity(son_1[0])
-                gops.mutation_complexity(son_2[0])
-            if np.random.random() <= mutation_setup['taskset']:
-                gops.mutation_taskset(son_1[0])
-                gops.mutation_taskset(son_2[0])
-            if np.random.random() <= mutation_setup['begin-end']:
-                gops.mutation_begin_end(son_1[0])
-                gops.mutation_begin_end(son_2[0])
-            # Testing the consistency of these shits
-            for key, jey in son_1[0].items():
-                if key not in ('inicio', 'fim'):
-                    if len(jey['in'][0]) == 3:
-                        if not jey['in'][1] and not jey['in'][2]:
-                            print('cagou3')
-                    elif len(jey['in'][0]) == 1:
-                        if not jey['in'][1]:
-                            print('cagou1')
-                    if len(jey['out'][0]) == 3:
-                        if not jey['out'][1] and not jey['out'][2]:
-                            print('cagou3out')
-                    elif len(jey['out'][0]) == 1:
-                        if not jey['out'][1]:
-                            print('cagou1out')
-            for kay, jay in son_2[0].items():
-                if kay not in ('inicio', 'fim'):
-                    if len(jay['in'][0]) == 3:
-                        if not jay['in'][1] and not jay['in'][2]:
-                            print('cagou3')
-                    elif len(jay['in'][0]) == 1:
-                        if not jay['in'][1]:
-                            print('cagou1')
-                    if len(jay['out'][0]) == 3:
-                        if not jay['out'][1] and not jay['out'][2]:
-                            print('cagou3out')
-                    elif len(jay['out'][0]) == 1:
-                        if not jay['out'][1]:
-                            print('cagou1out')
-
-            # Calculating fitness to the newborn
-            son_1[1] = gops.fitness(individuo=son_1[0], logs=logs, max_len_trace=max_len_trace, set_quant=set_quant, weights=weights_fit)[1]
-            son_2[1] = gops.fitness(individuo=son_2[0], logs=logs, max_len_trace=max_len_trace, set_quant=set_quant, weights=weights_fit)[1]
-            # Adding the new children to the new pop
-            new_pop.append(son_1)
-            new_pop.append(son_2)
-            if pop_exchange=='cohab':
-                new_pop.append(dad)
-                new_pop.append(mom)
-
-        # Doing the directed mutation
-        if mutation_setup['directed'] and (aux_gen > max_generations//2):
-            if np.random.random() <= mutation_setup['directed']:
-                indiv01 = gops.roulette_selection(new_pop)
-                indiv02 = gops.roulette_selection(new_pop)
-                gops.directed_mutation_dataset(indiv01[0], indiv02[0])
-
-        # Losing the reference to the old pop and keeping the new one
-        pop = new_pop
-        aux_gen += 1
-
-        # Collecting the data for the graphs
-        bar = [val[1]['f'] for val in pop]
-        min_fit = min(bar)
-        max_fit = max(bar)
-        average = sum(bar) / len(bar)
-        max_index = bar.index(max_fit)
-        # Sorting the population so i can get the best fitness
-        # sorted_pop = sorted(initial_pop, key=lambda t: t[1]['f'])
-        # Keeping the evolution stats
-        best_ind_evol.append(pop[max_index])
-        fit_evol.append([min_fit, max_fit, average])
-
-    return [best_ind_evol, fit_evol, pop]
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+import evolution_cycle as evol
 
 
 alphabetCM28 = ['A1', 'A2', 'A3', 'A4', 'A5', 'A6', 'A7', 'A8', 'A9']
@@ -168,7 +9,7 @@ logCM28 = {
         'D': ['A1', 'A2', 'A4', 'A3', 'A5', 'A6', 'A7', 'A9']}
 size_pop = 100
 pop_exchange = 'cohab' # c - cohab, k - kill ancestors
-max_generations = 10
+max_generations = 1000
 weights_fit = {'comp': 0.8, 'prec': 0.2}
 crossover_setup = {'points': 2, 'chance': 1}
 mutation_setup = {'logic':1, 'complex':1, 'taskset': 1, 'begin-end': 1, 'directed':1}  # each key holds the percent of chance that each mutation has of taking place
@@ -178,9 +19,9 @@ max_len_trace = max([len(foo) for foo in logCM28.values()]) * 4
 set_quant = len(logCM28) * 4
 exec_id = 'lol'
 
-result = evolution_cycle(alphabetCM28, logCM28, size_pop, pop_exchange, max_generations, weights_fit, crossover_setup, mutation_setup, selection_setup, elitism, max_len_trace, set_quant, exec_id)
+result = evol.evolution_cycle(alphabetCM28, logCM28, size_pop, pop_exchange, max_generations, weights_fit, crossover_setup, mutation_setup, selection_setup, elitism, max_len_trace, set_quant, exec_id)
 
 
 print(result[0][0], result[0][-1])
 
-# TODO Stopped here, need to fix the taskset mutation to ensure the individual always have a beginning/ending
+# TODO Stopped here, need to fix the taskset mutation to ensure the individual always have a beginning/ending'''
--- timeit_evolcycle.py
+++ timeit_evolcycle.py
@@ -2,7 +2,7 @@ import timeit as timeit
 
 result_time = timeit.timeit(
 '''
-import exec_evolution_cycle as evol
+import evolution_cycle as evol
 
 alphabetCM28 = ['A1', 'A2', 'A3', 'A4', 'A5', 'A6', 'A7', 'A8', 'A9']
 logCM28 = {
@@ -12,7 +12,7 @@ logCM28 = {
         'D': ['A1', 'A2', 'A4', 'A3', 'A5', 'A6', 'A7', 'A9']}
 size_pop = 100
 pop_exchange = 'cohab' # c - cohab, k - kill ancestors
-max_generations = 10
+max_generations = 1000
 weights_fit = {'comp': 0.8, 'prec': 0.2}
 crossover_setup = {'points': 2, 'chance': 1}
 mutation_setup = {'logic':1, 'complex':1, 'taskset': 1, 'begin-end': 1, 'directed':1}  # each key holds the percent of chance that each mutation has of taking place
@@ -25,6 +25,6 @@ exec_id = 'lol'
 result = evol.evolution_cycle(alphabetCM28, logCM28, size_pop, pop_exchange, max_generations, weights_fit, crossover_setup, mutation_setup, selection_setup, elitism, max_len_trace, set_quant, exec_id)
 
 '''
-,number = 2 )
+,number = 1 )
 
 print(result_time)
