7426cf6589f00af6045ba0a799dc1c8ed7b34d7d
lucas.silva1337@gmail.com <131429Alfaomeg@>
2017-10-05 17:45:03 -0300

    Last modifications, 03/10/2017
    Fixed error with empty input sets. The problem was in the fill_in_by_out function, i only checked simple structures to see if they were empty (so i could include them in the 'inicio' set of the individual. Maybe some of the mutation sets could cause this error too, but until now they seem to be ok
    
    Also, made a script to measure execution time

---
+++ timeit_evolcycle.py
@@ -0,0 +1,30 @@
+import timeit as timeit
+
+result_time = timeit.timeit(
+'''
+import exec_evolution_cycle as evol
+
+alphabetCM28 = ['A1', 'A2', 'A3', 'A4', 'A5', 'A6', 'A7', 'A8', 'A9']
+logCM28 = {
+        'A': ['A1', 'A2', 'A3', 'A4', 'A5', 'A6', 'A7', 'A9'],
+        'B': ['A1', 'A2', 'A4', 'A3', 'A5', 'A6', 'A8', 'A9'],
+        'C': ['A1', 'A2', 'A3', 'A4', 'A5', 'A6', 'A8', 'A9'],
+        'D': ['A1', 'A2', 'A4', 'A3', 'A5', 'A6', 'A7', 'A9']}
+size_pop = 100
+pop_exchange = 'cohab' # c - cohab, k - kill ancestors
+max_generations = 10
+weights_fit = {'comp': 0.8, 'prec': 0.2}
+crossover_setup = {'points': 2, 'chance': 1}
+mutation_setup = {'logic':1, 'complex':1, 'taskset': 1, 'begin-end': 1, 'directed':1}  # each key holds the percent of chance that each mutation has of taking place
+selection_setup = {'tournament':0, 'roulette':1}
+elitism = 0.1
+max_len_trace = max([len(foo) for foo in logCM28.values()]) * 4
+set_quant = len(logCM28) * 4
+exec_id = 'lol'
+
+result = evol.evolution_cycle(alphabetCM28, logCM28, size_pop, pop_exchange, max_generations, weights_fit, crossover_setup, mutation_setup, selection_setup, elitism, max_len_trace, set_quant, exec_id)
+
+'''
+,number = 2 )
+
+print(result_time)
