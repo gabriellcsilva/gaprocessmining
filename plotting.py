e300b5652272bc6749d0b7850da86c9761a10006
lucas.silva1337@gmail.com <131429Alfaomeg@>
2017-10-20 14:39:25 -0200

    Last modifications, 19/10/2017
    Alterations made to test the parameters

--- plotting.py
+++ plotting.py
@@ -1,21 +1,12 @@
 import matplotlib.pyplot as plt
 def plot_evolution(vetor_fitness, exec_id_name):
     # Plotting stuff
-    v_max = [i[1] for i in vetor_fitness]
     v_min = [i[0] for i in vetor_fitness]
+    v_max = [i[1] for i in vetor_fitness]
     v_avg = [i[2] for i in vetor_fitness]
-    # Figure for the average fitness evolution
-    plt.figure(1)
-    plt.plot(v_avg, label="Média", linewidth=2.0, color="orange")
-    plt.ylabel("Fitness")
-    plt.xlabel("Gerações")
-    plt.legend(bbox_to_anchor=(0., 1.02, 1., .102), loc=4, ncol=2, mode="expand", borderaxespad=0., prop={'size': 10})
-    # plt.ylim([-20, 1])
-    plt.draw()
-    name = exec_id_name + '-media-fit.png'
-    plt.savefig(name)
+
     # Figure for the maximum fitness evolution
-    plt.figure(2)
+    plt.figure(1)
     plt.plot(v_max, label="Maior fitness", linewidth=2.0, color="green")
     plt.ylabel("Fitness")
     plt.xlabel("Gerações")
@@ -24,8 +15,10 @@ def plot_evolution(vetor_fitness, exec_id_name):
     plt.draw()
     name = exec_id_name + '-maior-fit.png'
     plt.savefig(name)
+    plt.clf()
+
     # Figure for the minimum fitness evolution
-    plt.figure(3)
+    plt.figure(2)
     plt.plot(v_min, label="Menor Fitness", linestyle=':', linewidth=1.0, color="red")
     plt.ylabel("Fitness")
     plt.xlabel("Gerações")
@@ -35,3 +28,17 @@ def plot_evolution(vetor_fitness, exec_id_name):
     name = exec_id_name + '-pior-fit.png'
     plt.savefig(name)
     plt.clf()
+
+    # Figure for the average fitness evolution
+    plt.figure(3)
+    plt.plot(v_avg, label="Média", linewidth=2.0, color="orange")
+    plt.ylabel("Fitness")
+    plt.xlabel("Gerações")
+    plt.legend(bbox_to_anchor=(0., 1.02, 1., .102), loc=4, ncol=2, mode="expand", borderaxespad=0., prop={'size': 10})
+    # plt.ylim([-20, 1])
+    plt.draw()
+    name = exec_id_name + '-media-fit.png'
+    plt.savefig(name)
+    plt.clf()
+
+    return
