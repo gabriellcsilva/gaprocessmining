19c68ac0155d5aae1fcccc4275845b58ecab7682
lucas.silva1337@gmail.com <131429Alfaomeg@>
2017-10-20 15:30:45 -0200

    Last modifications, 20/10/2017
    Alterations made to test the parameters

--- plotting.py
+++ plotting.py
@@ -20,15 +20,16 @@ def plot_evolution(vetor_fitness, exec_id_name):
     # Figure for the minimum fitness evolution
     plt.figure(2)
     plt.plot(v_min, label="Menor Fitness", linestyle=':', linewidth=1.0, color="red")
+    plt.plot(v_avg, label="Média", linewidth=2.0, color="orange")
     plt.ylabel("Fitness")
     plt.xlabel("Gerações")
     plt.legend(bbox_to_anchor=(0., 1.02, 1., .102), loc=4, ncol=2, mode="expand", borderaxespad=0., prop={'size': 10})
     # plt.ylim([-20, 1])
     plt.draw()
-    name = exec_id_name + '-pior-fit.png'
+    name = exec_id_name + '-pior-med-fit.png'
     plt.savefig(name)
     plt.clf()
-
+    '''
     # Figure for the average fitness evolution
     plt.figure(3)
     plt.plot(v_avg, label="Média", linewidth=2.0, color="orange")
@@ -39,6 +40,5 @@ def plot_evolution(vetor_fitness, exec_id_name):
     plt.draw()
     name = exec_id_name + '-media-fit.png'
     plt.savefig(name)
-    plt.clf()
-
+    plt.clf()'''
     return
